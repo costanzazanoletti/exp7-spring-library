da4c936cf202df052e765308f764b6895af218dd
costanzazanoletti <78044829+costanzazanoletti@users.noreply.github.com>
2023-11-10 13:01:12 +0100

    Live Coding show book details, search con custom query

--- src/main/java/com/experis/course/springlibrary/controller/BookController.java
+++ src/main/java/com/experis/course/springlibrary/controller/BookController.java
@@ -3,11 +3,16 @@ package com.experis.course.springlibrary.controller;
 import com.experis.course.springlibrary.model.Book;
 import com.experis.course.springlibrary.repository.BookRepository;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.server.ResponseStatusException;
 
 @Controller
 @RequestMapping("/books")
@@ -20,11 +25,42 @@ public class BookController {
 
   // metodo che mostra la lista di tutti i libri
   @GetMapping
-  public String index(Model model) {
-    // bookRepository recupera da database la lista di tutti i libri
-    List<Book> bookList = bookRepository.findAll();
+  public String index(@RequestParam Optional<String> search,
+      Model model) {
+    List<Book> bookList;
+
+    if (search.isPresent()) {
+      // se il parametro di ricerca è presente filtro la lista dei libri
+      bookList = bookRepository.findByTitleContainingIgnoreCaseOrAuthorsContainingIgnoreCase(
+          search.get(),
+          search.get());
+    } else {
+      // altrimenti prendo tutti i libri non filtrati
+      // bookRepository recupera da database la lista di tutti i libri
+      bookList = bookRepository.findAll();
+    }
+
     // passo al template la lista di libri
     model.addAttribute("bookList", bookList);
     return "books/list";
+  }
+
+  // metodo che mostra i dettagli di un libro preso per id
+  @GetMapping("/show/{id}")
+  public String show(@PathVariable Integer id, Model model) {
+    Optional<Book> result = bookRepository.findById(id);
+    // verifico se il risultato è presente
+    if (result.isPresent()) {
+      // passo al template l'oggetto Book
+      model.addAttribute("book", result.get());
+      return "books/show";
+    } else {
+      // se non ho trovato il libro sollevo un'eccezione
+      throw new ResponseStatusException(HttpStatus.NOT_FOUND, "book with id " + id + " not found");
+    }
+/*    model.addAttribute("book", bookRepository.findById(id).orElseThrow(
+        () -> new ResponseStatusException(HttpStatus.NOT_FOUND,
+            "book with id " + id + " not found")));
+    return "books/show";*/
   }
 }
--- src/main/java/com/experis/course/springlibrary/repository/BookRepository.java
+++ src/main/java/com/experis/course/springlibrary/repository/BookRepository.java
@@ -1,9 +1,14 @@
 package com.experis.course.springlibrary.repository;
 
 import com.experis.course.springlibrary.model.Book;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 // nei generics devo mettere il tipo di dato dell'entità e il tipo di dato dell'attributo annotato come @Id
 public interface BookRepository extends JpaRepository<Book, Integer> {
+
+  // query custom per ricerca su title e authors
+  List<Book> findByTitleContainingIgnoreCaseOrAuthorsContainingIgnoreCase(String titleKeyword,
+      String authorsKeyword);
 
 }
